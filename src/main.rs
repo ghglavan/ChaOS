50e7076a616017ad128f88e6e2cab1f465684a7a
Gheorghe Florian GLAVAN <gglavan@bitdefender.com>
2021-04-09 23:15:07 +0300

    add fp handling for the context switch

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use cortex_m::interrupt;
 use cortex_m::Peripherals;
 use cortex_m::peripheral::{SYST, syst::SystClkSource};
 
+
 const TASKS: usize = 3;
 const TCB_STACK_SIZE: usize = 300;
 const QUANTA_US: u32 = 100 * 1_000; // 10 ms 
@@ -36,41 +37,59 @@ static mut TASK0_COUNTER: u32 = 0;
 static mut TASK1_COUNTER: u32 = 0;
 static mut TASK2_COUNTER: u32 = 0;
 
-static mut TCBS: [TaskControlBlock; TASKS] = [TaskControlBlock{stack_ptr: 0 as *mut u32}; TASKS];
+static mut TCBS: [TaskControlBlock; TASKS] = [TaskControlBlock {stack_ptr: 0 as *mut u32}; TASKS];
 static mut CURRENT_TCB_INDEX: usize = 2;
+static mut NEXT_TCB_INDEX: usize = 0;
 
 static mut TCB_STACK0: [u32; TCB_STACK_SIZE] = [0; TCB_STACK_SIZE];
 static mut TCB_STACK1: [u32; TCB_STACK_SIZE] = [0; TCB_STACK_SIZE];
 static mut TCB_STACK2: [u32; TCB_STACK_SIZE] = [0; TCB_STACK_SIZE];
 
-fn do_switch() {
-     unsafe {
-        let current_tcb = &mut TCBS[CURRENT_TCB_INDEX];
-        CURRENT_TCB_INDEX = (CURRENT_TCB_INDEX + 1) % TASKS;
-        let next_tcb = &mut TCBS[CURRENT_TCB_INDEX];
-
-        asm!(
-             "mrs {0}, psp", // load psp to r0
-             "stmdb {0}!, {{r4-r11}}", // push r4-r11 adn set r0 to then new stack
-             "ldmia {1}!, {{r4-r11}}",
-             "msr psp, {1}",
-             inout(reg) current_tcb.stack_ptr, inout(reg) next_tcb.stack_ptr
-        );
-    }
-}
-
 #[exception]
 fn SysTick() {
-    interrupt::disable();
-    do_switch();
-    unsafe {interrupt::enable();}
-    
+    cortex_m::peripheral::SCB::set_pendsv();
+}
+
+
+// wo dont use the classic exception here because we ned the lr in order to check the
+// fp status of the task. Using the #[exception] macro will generate
+//
+// #[doc(hidden)]
+// #[export_name = "PendSV"]
+// pub unsafe extern "C" fn __cortex_m_rt_PendSV_trampoline() {
+//     __cortex_m_rt_PendSV()
+// }
+// fn __cortex_m_rt_PendSV() {
+//		... our function ...
+// }
+// 
+// 
+// this does not help us becase the trampoline is going to change the lr so the check
+// 'tst  lr, #0x10' will give us a bogus value because it uses the trampoline lr
+#[export_name = "PendSV"]
+pub unsafe extern "C" fn PendSV() {
     unsafe {
-        asm!("ldr lr, ={LD}", // load lr with 0xfffffff9 in order to return from psp 
-             "pop {{r0-r3}}", // pop the top 4 values so we dont fill the main stack
-             "bx lr", // branch to lr so we finnish the context switch
-             LD = const 0xfffffffdu32
-             ); 
+        let prev_tcb = &mut TCBS[CURRENT_TCB_INDEX];
+        CURRENT_TCB_INDEX = (CURRENT_TCB_INDEX + 1) % TASKS;
+        let next_tcb = &mut TCBS[CURRENT_TCB_INDEX];
+     
+        asm!("mrs       r0, PSP",
+             "tst       lr, #0x10",
+             "it        eq",
+             "vstmdbeq  r0!, {{s16-s31}}",
+             "mov       r2, lr",
+             "mrs       r3, control",
+             "stmdb     r0!, {{r2-r11}}",
+             "ldmia     r1!, {{r2-r11}}",
+             "mov       lr, r2",
+             "msr       control, r3",
+             "isb",
+             "tst       lr, #0x10",
+             "it        eq",
+             "vldmiaeq  r1!, {{s16-s31}}",
+             "msr       psp, r1",
+             inout("r0") prev_tcb.stack_ptr, inout("r1") next_tcb.stack_ptr
+            );
     }
 }
 
@@ -103,9 +122,9 @@ fn setup() {
 
     unsafe {
         
-        TCBS[0].stack_ptr = (&mut TCB_STACK0).as_mut_ptr().offset((TCB_STACK_SIZE - 16) as isize); 
-        TCBS[1].stack_ptr = (&mut TCB_STACK1).as_mut_ptr().offset((TCB_STACK_SIZE - 16) as isize); 
-        TCBS[2].stack_ptr = (&mut TCB_STACK2).as_mut_ptr().offset((TCB_STACK_SIZE - 8) as isize); 
+        TCBS[0].stack_ptr = (&mut TCB_STACK0).as_mut_ptr().offset((TCB_STACK_SIZE - 18) as isize); 
+        TCBS[1].stack_ptr = (&mut TCB_STACK1).as_mut_ptr().offset((TCB_STACK_SIZE - 18) as isize); 
+        TCBS[2].stack_ptr = (&mut TCB_STACK2).as_mut_ptr().offset((TCB_STACK_SIZE - 18) as isize); 
 
         TCB_STACK0[TCB_STACK_SIZE - 1] = 0x0100_0000;
         TCB_STACK1[TCB_STACK_SIZE - 1] = 0x0100_0000;
@@ -115,12 +134,18 @@ fn setup() {
         TCB_STACK1[TCB_STACK_SIZE - 2] = task1 as u32;
         TCB_STACK2[TCB_STACK_SIZE - 2] = task2 as u32;
         
-        CURRENT_TCB_INDEX = 2;
+        TCB_STACK0[TCB_STACK_SIZE - 17] = 0x3; // initial CONTROL: unprivileged, PSP, no fp
+        TCB_STACK1[TCB_STACK_SIZE - 17] = 0x3; // initial CONTROL: unprivileged, PSP, no fp
+        TCB_STACK2[TCB_STACK_SIZE - 17] = 0x3; // initial CONTROL: unprivileged, PSP, no fp
+
+        
+        TCB_STACK0[TCB_STACK_SIZE - 18] = 0xFFFFFFFD;
+        TCB_STACK1[TCB_STACK_SIZE - 18] = 0xFFFFFFFD;
+        TCB_STACK2[TCB_STACK_SIZE - 18] = 0xFFFFFFFD;
+
+        CURRENT_TCB_INDEX = 0;
        
-        let current_tcb = &TCBS[CURRENT_TCB_INDEX];
-        // set psp to task2 so we can store psp in the context switch
-        asm!("msr psp, {}", in(reg) current_tcb.stack_ptr);
-    }
+	}
 
 
     unsafe {
@@ -135,6 +160,20 @@ fn setup() {
     syst.clear_current();
     syst.enable_counter();
     syst.enable_interrupt();
+    
+    unsafe {
+        let current_tcb = &TCBS[CURRENT_TCB_INDEX];
+         
+        let ctrl = 0x3;
+        asm!(
+            "msr psp, {0}",
+            "msr control, {1}",
+            in(reg) current_tcb.stack_ptr,
+            in(reg) ctrl
+            );
+    }
+       
+    task0();
 
 }
 
