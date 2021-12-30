c4a9ee5cb349009e88838018c9d0098faabbb5c3
ghglavan <george.glavan27@gmail.com>
2021-12-30 23:01:08 +0200

    add asm_sys feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 #![no_std]
 #![feature(asm)]
+#![feature(asm_sym)]
 #![feature(global_asm)]
 #![feature(naked_functions)]
 
 pub use chaos_macros::os;
-pub mod scheduler;
-pub mod task;
-pub mod os;
-pub mod chaos;
-pub mod syscalls;
 pub mod asm;
+pub mod chaos;
 mod exceptions;
+pub mod os;
+pub mod scheduler;
+pub mod syscalls;
 mod systick;
+pub mod task;
